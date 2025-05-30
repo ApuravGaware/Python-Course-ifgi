a5c9302322252dd9dca69ec7fa84d0c052acd8fa
Apurav Santosh Gaware <90237593+ApuravGaware@users.noreply.github.com>
2025-05-30 16:53:33 +0200

    Update calculator.py

--- exercise_3/easy_shopping/calculator.py
+++ exercise_3/easy_shopping/calculator.py
@@ -1,1 +1,20 @@
+# Calculator.py to do mathematical operations
+class Calculator:
+    # Addition
+    def add(self, a, b):
+        return a + b
 
+    # Subtraction
+    def sub(self, a, b):
+        return a - b
+
+    # Multiplication 
+    def mul(self, a, b):
+        return a * b
+
+    # Division
+    def div(self, a, b):
+        if b != 0:
+            return a / b
+        else:
+            return ("Number can't be divided by zero.")
