e62cd328a5f1d210878005ef6aa7a7754b4436f7
Apurav Santosh Gaware <90237593+ApuravGaware@users.noreply.github.com>
2025-05-30 16:54:16 +0200

    Update shopping.py

--- exercise_3/easy_shopping/shopping.py
+++ exercise_3/easy_shopping/shopping.py
@@ -1,1 +1,26 @@
+# Shopping.py for making a cart
+class Cart:
+    def __init__(self):
+        self.items = {}
 
+    # Add item
+    def add_item(self, item_name, qty):
+        if item_name in self.items:
+            self.items[item_name] += qty
+        else:
+            self.items[item_name] = qty
+
+    # Remove item 
+    def remove_item(self, item_name):
+        if item_name in self.items:
+            del self.items[item_name]
+        else:
+            print(f"Item '{item_name}' not found in cart.")
+
+    #show
+    def show_cart(self):
+        return self.items
+
+    # total quantity of items
+    def quantity(self):
+        return sum(self.items.values())
