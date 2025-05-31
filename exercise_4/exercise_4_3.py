376d99f2de0548d4a82ce26730e016b328ed6910
Apurav Santosh Gaware <90237593+ApuravGaware@users.noreply.github.com>
2025-05-31 22:55:22 +0200

    Update exercise_4_3.py

--- exercise_4/exercise_4_3.py
+++ exercise_4/exercise_4_3.py
@@ -1,1 +1,28 @@
+import os
+from qgis.core import (
+    QgsProject,
+    QgsVectorLayer
+)
+
+folder_path = '/Users/apurav/Downloads/Muenster' 
+
+project = QgsProject.instance()
+project.clear() 
+
+#Getting all shapefiles in the folder
+shapefiles = [f for f in os.listdir(folder_path) if f.endswith('.shp')]
+
+#Load every shapefile into project
+for file in shapefiles:
+    full_path = os.path.join(folder_path, file)
+    layer_name = os.path.splitext(file)[0]  
+    layer = QgsVectorLayer(full_path, layer_name, "ogr")
+    
+    if layer.isValid():
+        QgsProject.instance().addMapLayer(layer)
+    else:
+        print(f"Failed to load: {file}")
+
+#Save project
+project.write('/Users/apurav/Desktop/myFirstProject.qgz')  
 
